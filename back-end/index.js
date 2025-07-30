dc44d5f22ee339f8c1b83336f4befd9b0961b2e3
renukakathirvel <renukakathirvel18@gmail.com>
2025-07-30 12:30:15 +0530

    adding .env in frontend

--- back-end/index.js
+++ back-end/index.js
@@ -30,7 +30,7 @@ app.use(helmet());
 app.use('/api/v1', userRoutes);
 app.use('/api/v1/payments', paymentRoutes);
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://spontaneous-kitten-e9133f.netlify.app/',
   credentials: true,
 }));
 
